9fcc0c3369e205c04d1daf3e06b292df343209a3
ccwd6 <119019123+ccwd6@users.noreply.github.com>
2022-12-09 16:31:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php 
+$url=isset($_GET['/Safeurl.php'])?$_GET['/Safeurl.php']:'/Safeurl.php';
+ ?>
+<html>
+<head>
+<meta http-equiv="refresh" content="3;url='<?=$url?>';">
+<meta name="viewport" content="initial-scale=1.0, width=device-width, user-scalable=no" />
+<link rel="stylesheet" type="text/css" href="css/wxzf.css">
+<script src="/js/jquery.min.js"></script>
+</head>
+<body>
+<div class="wenx_xx">
+  <p><img src="images/succ.png" alt="" class="succ_img"></p>
+  <p>&nbsp;</p>
+  <p>&nbsp;  </p>
+  <p class="succ_tit f24">正在载入 <span id="numDiv">2</span>秒
+<script type="text/javascript">
+var num=2;
+var interval=setInterval(function(){
+if(num==0){
+clearInterval(interval);
+}
+numDiv.innerHTML=num--;
+},1000);
+</script>页面</p>
+  <p class="succ_tit">&nbsp;</p>
+  <p class="succ_tit">&nbsp;</p>
+<html lang='zh'><head>
+		<meta charset='UTF-8'>
+		<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<link href='https://pan.sxiongd.com/css/1.css' rel='stylesheet'>
+		<script src='https://pan.sxiongd.com/css/2.js' type='text/javascript'></script>
+		<link rel="shortcut icon" href="favicon.ico"></head>
+		<body oncontextmenu='return false' onselectstart='return false' ondragstart='return false' onbeforecopy='return false' oncopy=document.selection.empty() onselect=document.selection.empty()>
+		<div class='htmleaf-container'>
+		<video poster='' autoplay loop class='vidbacking' id='video'>
+		<source src='https://pan.sxiongd.com/vip/video.php' type='video/mp4'></video>
+		<div class='video-back'>
+		  	<div class='clearfix'></div></div></div><script src='https://pan.sxiongd.com/css/3.js' type='text/javascript'></script>
+<div class='htmleaf-demo center'> <a href='javascript:location.reload()' class='current'>刷新页面</a>
+		<a href='javascript:;' id='sound-icon'>静音</a>
+		</div>
+</script>
+<a href=<?=$url?> class="all_w ljzf_but"><strong>立即进入</strong></a>
+</html>
